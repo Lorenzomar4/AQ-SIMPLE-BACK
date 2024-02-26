c29befde340413f32d614351b128d2320a13a985
lorenzomar4 <m100_98@hotmail.com.ar>
2024-02-26 13:09:29 -0300

    desplegable independiente

--- src/main/java/com/lorenzomar3/AQ/model/AResponder/TiposDePreguntas/DesplegableIndependiente.java
+++ src/main/java/com/lorenzomar3/AQ/model/AResponder/TiposDePreguntas/DesplegableIndependiente.java
@@ -1,4 +1,48 @@
 package com.lorenzomar3.AQ.model.AResponder.TiposDePreguntas;
 
-public class DesplegableIndependiente {
+import com.lorenzomar3.AQ.model.AResponder.Pregunta;
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Transient;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class DesplegableIndependiente extends Pregunta<List<RespuestaDeDesplegableIndependiente>> {
+
+
+    @OneToMany
+    @JoinColumn(name = "id_pregunta_desplegable_ind")
+    List<SeleccionUnica> listaDePreguntasConOpcionUnicas;
+
+
+    @Transient
+    HashMap<Long, Long> preguntaConSuIdDeLaRespuestaCorrespondiente = new HashMap<>();
+
+    @PostConstruct
+    public void init() {
+
+        listaDePreguntasConOpcionUnicas.forEach(pregunta -> {
+                    preguntaConSuIdDeLaRespuestaCorrespondiente.put(pregunta.getId(), pregunta.getIdDeLaRespuestaCorrecta());
+                }
+        );
+
+    }
+
+    @Override
+    public boolean laRespuestaEsCorrecta(List<RespuestaDeDesplegableIndependiente> listaRespuesta) {
+        return listaRespuesta.stream().allMatch(resp ->
+                preguntaConSuIdDeLaRespuestaCorrespondiente.get(resp.getIdDeLaPregunta())
+                        .equals(resp.getIdDeLaRespuestaSeleccionada())
+        );
+    }
+
+
 }
---
+++ src/main/java/com/lorenzomar3/AQ/model/AResponder/TiposDePreguntas/RespuestaDeDesplegableIndependiente.java
@@ -0,0 +1,15 @@
+package com.lorenzomar3.AQ.model.AResponder.TiposDePreguntas;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Getter
+public class RespuestaDeDesplegableIndependiente {
+
+    @Getter
+    Long idDeLaPregunta;
+
+    Long idDeLaRespuestaSeleccionada;
+
+
+}
